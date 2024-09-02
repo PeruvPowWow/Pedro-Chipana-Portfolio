afc98853a5aff2af9d3a54c78164c24320d4881f
Papito <170674313+PeruvPowWow@users.noreply.github.com>
2024-09-02 00:57:13 -0600

    added about me section and need to work on getting the picture on there

---
+++ react_portfolio/src/pages/AboutMe.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function AboutMe() {
+    return (
+        <section>
+            <h2>About Me</h2>
+            <img src="/path-to=your-image.jpg" alt="Pedro Chipana" />
+            <p> Hello! My name is Pedro, and I graduated from the University of Utah with a focus on pursuing a career in the health industry. However, unforeseen circumstances necessitated a shift in my career path. Over the past few years, I have worked in the technology sector, primarily in sales. Although I had not initially envisioned a long-term career in this field, I have consistently been intrigued by both front-end and back-end development processes. I believe now is the opportune time to embark on this new journey and further explore my passion for development.
+                In addition to my professional experiences, I have actively engaged in various online courses and coding boot camps to hone my skills. This continuous learning has not only equipped me with a solid foundation in programming but also fueled my enthusiasm for creating innovative solutions. I am excited to leverage this knowledge and transition into a role where I can contribute to dynamic projects and collaborate with like-minded professionals in the tech industry.
+            </p>
+        </section>
+    );
+}
+
+export default AboutMe;
