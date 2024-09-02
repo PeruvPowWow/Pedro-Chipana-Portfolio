dfa214db0888d6200d3886eb754bcfa2ddf17401
Papito <170674313+PeruvPowWow@users.noreply.github.com>
2024-09-01 23:44:29 -0600

    added iimports to app.jsx and the route paths

--- react_portfolio/src/App.jsx
+++ react_portfolio/src/App.jsx
@@ -6,6 +6,19 @@ import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
 import Resume from './pages/Resume';
 
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<AboutMe/>} />
+        <Route path="/portfolio" element={<Portfolio/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/resume" element={<Resume/>} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
 
-
-export default App
+export default App;
