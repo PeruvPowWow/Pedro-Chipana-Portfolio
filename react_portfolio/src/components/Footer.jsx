f5e1ce61c0c9a37eeb81744c60adcf52c8f832dc
Papito <170674313+PeruvPowWow@users.noreply.github.com>
2024-09-02 00:52:59 -0600

    added content to the footer file

--- react_portfolio/src/components/Footer.jsx
+++ react_portfolio/src/components/Footer.jsx
@@ -1,1 +1,14 @@
-e
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+        <p>
+            <a href="https://github.com/PeruvPowWow" target="_blank" rel="noopener noreferrer">GitHub</a> |
+            <a href="https://www.linkedin.com/in/pedro-chipana-984b5a119/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        </p>
+        </footer>
+    );
+    }
+
+    export default Footer;
