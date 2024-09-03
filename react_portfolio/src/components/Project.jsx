28fc16c05cb89b169c150a01ae9bf6fbe49324f2
Papito <170674313+PeruvPowWow@users.noreply.github.com>
2024-09-03 14:10:24 -0600

    added new content to project and portfolio

--- react_portfolio/src/components/Project.jsx
+++ react_portfolio/src/components/Project.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function Project ({ title, image, deployedLink, repoLink}) {
     return (
-        <div>
+        <div className="project">
             <h3>{title}</h3>
-            <img src="{image}" alt="{title}" />
+            <img src="{image}" alt="{title}" className="project-image" />
             <p>
-             <a href={deployedLink} target="_blank" rel="noopener noreferrer">Deployed App</a> | 
+             <a href={deployedLink} target="_blank" rel="noopener noreferrer">Deployed App</a> |{" "} 
              <a href={repoLink} target="_blank" rel="noopener noreferrer">GitHub Repo</a>
             </p>
         </div>
--- react_portfolio/src/pages/Portfolio.jsx
+++ react_portfolio/src/pages/Portfolio.jsx
@@ -5,25 +5,25 @@ function Portfolio() {
   const projects = [
       {
           title: "HTML/CSS Code Project",
-          image: "/Users/pedrochipana/Desktop/BOOTCAMP/Pedro-Chipana-Portfolio/react_portfolio/src/assets/images/digital-marketing-meeting.jpg",
+          image: require("/Users/pedrochipana/Desktop/BOOTCAMP/Pedro-Chipana-Portfolio/react_portfolio/src/assets/images/digital-marketing-meeting.jpg"),
           deployedLink: "https://peruvpowwow.github.io/homework-challenge-1/",
           repoLink: "https://github.com/PeruvPowWow/homework-challenge-1/tree/main"
       },
       {
             title: "Employee Payroll Tracker",
-            image: "/Users/pedrochipana/Desktop/BOOTCAMP/Pedro-Chipana-Portfolio/react_portfolio/src/assets/images/Employment Payroll Tracker.jpg",
+            image: require("/Users/pedrochipana/Desktop/BOOTCAMP/Pedro-Chipana-Portfolio/react_portfolio/src/assets/images/Employment Payroll Tracker.jpg"),
             deployedLink: "https://peruvpowwow.github.io/Employment-Payroll-Tracker/",
             repoLink: "https://github.com/PeruvPowWow/Employment-Payroll-Tracker"
       },
     {
             title: "My First Blog",
-            image: "/Users/pedrochipana/Desktop/BOOTCAMP/Pedro-Chipana-Portfolio/react_portfolio/src/assets/images/100-web-apis-challenge-demo.gif",
+            image: require("/Users/pedrochipana/Desktop/BOOTCAMP/Pedro-Chipana-Portfolio/react_portfolio/src/assets/images/100-web-apis-challenge-demo.gif"),
             deployedLink: "https://peruvpowwow.github.io/My-First-Blog/",
             repoLink: "https://github.com/PeruvPowWow/My-First-Blog"
     },
     {
             title: "ProjectPAL",
-            image: "/Users/pedrochipana/Desktop/BOOTCAMP/Pedro-Chipana-Portfolio/react_portfolio/src/assets/images/projectpalheader-dark.jpg",
+            image: require("/Users/pedrochipana/Desktop/BOOTCAMP/Pedro-Chipana-Portfolio/react_portfolio/src/assets/images/projectpalheader-dark.jpg"),
             deployedLink: "https://peruvpowwow.github.io/ProjectPal/",
             repoLink: "https://github.com/PeruvPowWow/ProjectPal"
     },
@@ -33,7 +33,13 @@ function Portfolio() {
     <section>
         <h2>Portfolio</h2>
         {projects.map((project, index) => (
-            <Project key={index} project={project} />
+            <Project 
+            key={index}
+            title={project.title}
+            image={project.image}
+            deployedLink={project.deployedLink}
+            repoLink={project.repoLink}
+             />
         ))}
     </section>
   );
