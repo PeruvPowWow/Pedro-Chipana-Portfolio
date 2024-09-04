8e14db1c6bcdf3caa12e87d065c7cff4a8ff88a3
Papito <170674313+PeruvPowWow@users.noreply.github.com>
2024-09-03 23:06:07 -0600

    fixing iissues to all files

--- react_portfolio/src/pages/Portfolio.jsx
+++ react_portfolio/src/pages/Portfolio.jsx
@@ -1,46 +1,44 @@
-import React from "react";
+// import React from "react";
 import Project from "../components/Project";
+import image1 from "../assets/images/digital-marketing-meeting.jpg";
+import image2 from "../assets/images/Employment Payroll Tracker.jpg";
+import image3 from "../assets/images/100-web-apis-challenge-demo.gif";
+import image4 from "../assets/images/projectpalheader-dark.jpg";
 
 function Portfolio() {
   const projects = [
-      {
-          title: "HTML/CSS Code Project",
-          image: require("/Users/pedrochipana/Desktop/BOOTCAMP/Pedro-Chipana-Portfolio/react_portfolio/src/assets/images/digital-marketing-meeting.jpg"),
-          deployedLink: "https://peruvpowwow.github.io/homework-challenge-1/",
-          repoLink: "https://github.com/PeruvPowWow/homework-challenge-1/tree/main"
-      },
-      {
-            title: "Employee Payroll Tracker",
-            image: require("/Users/pedrochipana/Desktop/BOOTCAMP/Pedro-Chipana-Portfolio/react_portfolio/src/assets/images/Employment Payroll Tracker.jpg"),
-            deployedLink: "https://peruvpowwow.github.io/Employment-Payroll-Tracker/",
-            repoLink: "https://github.com/PeruvPowWow/Employment-Payroll-Tracker"
-      },
     {
-            title: "My First Blog",
-            image: require("/Users/pedrochipana/Desktop/BOOTCAMP/Pedro-Chipana-Portfolio/react_portfolio/src/assets/images/100-web-apis-challenge-demo.gif"),
-            deployedLink: "https://peruvpowwow.github.io/My-First-Blog/",
-            repoLink: "https://github.com/PeruvPowWow/My-First-Blog"
+      title: "HTML/CSS Code Project",
+      image: image1,
+      deployedLink: "https://peruvpowwow.github.io/homework-challenge-1/",
+      repoLink: "https://github.com/PeruvPowWow/homework-challenge-1/tree/main",
     },
     {
-            title: "ProjectPAL",
-            image: require("/Users/pedrochipana/Desktop/BOOTCAMP/Pedro-Chipana-Portfolio/react_portfolio/src/assets/images/projectpalheader-dark.jpg"),
-            deployedLink: "https://peruvpowwow.github.io/ProjectPal/",
-            repoLink: "https://github.com/PeruvPowWow/ProjectPal"
+      title: "Employee Payroll Tracker",
+      image: image2,
+      deployedLink: "https://peruvpowwow.github.io/Employment-Payroll-Tracker/",
+      repoLink: "https://github.com/PeruvPowWow/Employment-Payroll-Tracker",
+    },
+    {
+      title: "My First Blog",
+      image: image3,
+      deployedLink: "https://peruvpowwow.github.io/My-First-Blog/",
+      repoLink: "https://github.com/PeruvPowWow/My-First-Blog",
+    },
+    {
+      title: "ProjectPAL",
+      image: image4,
+      deployedLink: "https://peruvpowwow.github.io/ProjectPal/",
+      repoLink: "https://github.com/PeruvPowWow/ProjectPal",
     },
   ];
 
   return (
     <section>
-        <h2>Portfolio</h2>
-        {projects.map((project, index) => (
-            <Project 
-            key={index}
-            title={project.title}
-            image={project.image}
-            deployedLink={project.deployedLink}
-            repoLink={project.repoLink}
-             />
-        ))}
+      <h2>Portfolio</h2>
+      {projects.map((project, index) => (
+        <Project key={index} project={project} />
+      ))}
     </section>
   );
 }
--- react_portfolio/src/pages/Resume.jsx
+++ react_portfolio/src/pages/Resume.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 
 function Resume() {
     const frontEndProficiencies = ['HTML', 'CSS', 'JavaScript', 'React', 'Bootstrap'];
