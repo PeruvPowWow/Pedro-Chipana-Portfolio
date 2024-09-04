80e696868f4d14b7b88ce49fc1872c7bf519a750
Papito <170674313+PeruvPowWow@users.noreply.github.com>
2024-09-03 23:19:42 -0600

    fixing more bugs

--- react_portfolio/src/components/Project.jsx
+++ react_portfolio/src/components/Project.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+// import React from "react";
+import PropTypes from 'prop-types';
 
-function Project ({ title, image, deployedLink, repoLink}) {
+function Project({ title, image, deployedLink, repoLink }) {
     return (
         <div className="project">
             <h3>{title}</h3>
-            <img src="{image}" alt="{title}" className="project-image" />
+            <img src={image} alt={title} className="project-image" />
             <p>
-             <a href={deployedLink} target="_blank" rel="noopener noreferrer">Deployed App</a> |{" "} 
-             <a href={repoLink} target="_blank" rel="noopener noreferrer">GitHub Repo</a>
+                <a href={deployedLink} target="_blank" rel="noopener noreferrer">Deployed App</a> |{" "}
+                <a href={repoLink} target="_blank" rel="noopener noreferrer">GitHub Repo</a>
             </p>
         </div>
-  );
+    );
 }
+
+// PropTypes for validation
+Project.propTypes = {
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    deployedLink: PropTypes.string.isRequired,
+    repoLink: PropTypes.string.isRequired,
+};
 
 export default Project;
