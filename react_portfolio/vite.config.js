7a8230db47b6da666e87c4cc6a450fcb5f65639f
Papito <170674313+PeruvPowWow@users.noreply.github.com>
2024-09-10 08:31:40 -0600

    more fixes

--- react_portfolio/vite.config.js
+++ react_portfolio/vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   root: './',
+  build: {
+    rollupOptions: {
+      input: './index.html', // Make sure Vite uses index.html as the entry point
+      external: ['/src/main.jsx'], // Exclude this file from Rollup bundling
+    },
+  },
 })
